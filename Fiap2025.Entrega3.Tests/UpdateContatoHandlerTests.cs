0c049788216f2c4235f810504e4ac7b36537bc3f
Guilherme Coelho Garnizet <61944236+wagnergarnizet@users.noreply.github.com>
2025-03-16 21:58:10 -0300

    ok

---
+++ Fiap2025.Entrega3.Tests/UpdateContatoHandlerTests.cs
@@ -0,0 +1,37 @@
+﻿using Fiap2025.Entrega3.Application.Commands;
+using Fiap2025.Entrega3.Application.Handlers;
+using Fiap2025.Entrega3.Domain.Entities;
+using Fiap2025.Entrega3.Domain.Interfaces;
+using Moq;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Fiap2025.Entrega3.Tests
+{
+    public class UpdateContatoHandlerTests
+    {
+        [Fact]
+        public async Task Handle_ShouldUpdateContato()
+        {
+            // Arrange
+            var repositoryMock = new Mock<IContatoRepository>();
+            var handler = new UpdateContatoHandler(repositoryMock.Object);
+            var command = new UpdateContatoCommand
+            {
+                Id = Guid.NewGuid(),
+                Name = "Wagner Garnizet",
+                Email = "wagnergarnzet@hotmail.com",
+                Phone = "967918795",
+                AreaCode = "11"
+            };
+
+            // Act
+            await handler.Handle(command, CancellationToken.None);
+
+            // Assert
+            repositoryMock.Verify(r => r.UpdateContatoAsync(It.IsAny<Contato>()), Times.Once);
+        }
+    }
+}
